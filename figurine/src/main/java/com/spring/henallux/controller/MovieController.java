d41d94a9d87b30e9e342055be242382c165913ea
louisvdd <etu28841@student.henallux.be>
2016-11-19 10:30:36 +0100

    sdf

--- figurine/src/main/java/com/spring/henallux/controller/MovieController.java
+++ figurine/src/main/java/com/spring/henallux/controller/MovieController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.spring.henallux.model.*;
- 
+
 @Controller
 @RequestMapping(value="/movie")
 public class MovieController 
